f2f53a6d42bc3bf9c91e37b0f09465991f26b469
krishnab <cyclotomiq@gmail.com>
2016-05-10 00:45:02 -0700

    updated for build of version 0.1.0

--- pyugend/__init__.py
+++ pyugend/__init__.py
@@ -1,7 +1,8 @@
 __version__ = '0.1.0'
 
 __all__ = ['Base_model', 'Replication_model', 'Mod_Stoch_VSHP',
-           'Mod_Stoch_FSHP', 'Mod_Stoch_FBHP', 'Mod_Stoch_FBPH']
+           'Mod_Stoch_FSHP', 'Mod_Stoch_FBHP', 'Mod_Stoch_FBPH',
+           'Mod_Stoch_FBHP_alt', 'Mod_Stoch_FBPH_alt']
 
 from pyugend.Models import Base_model
 from pyugend.ReplicationModel import Replication_model
@@ -9,6 +10,10 @@ from pyugend.Mod_Stoch_VSHP import Mod_Stoch_VSHP
 from pyugend.Mod_Stoch_FSHP import Mod_Stoch_FSHP
 from pyugend.Mod_Stoch_FBHP import Mod_Stoch_FBHP
 from pyugend.Mod_Stoch_FBPH import Mod_Stoch_FBPH
+from pyugend.Mod_Stoch_FBHP_alt import Mod_Stoch_FBHP_alt
+from pyugend.Mod_Stoch_FBPH_alt import Mod_Stoch_FBPH_alt
+
+
 
 
 
