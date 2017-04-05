2d1287c15f018713459e8755cbec8a55817a9cca
krishnab <cyclotomiq@gmail.com>
2017-04-04 17:15:30 -0700

    updating __init__.py for paths to classes.

--- pyugend/__init__.py
+++ pyugend/__init__.py
@@ -4,13 +4,14 @@ __all__ = ['Base_model',
            'Replication_model',
            'Mod_Stoch_FBHP',
            'Mod_Stoch_FBPH',
-           'Mod_Validate_Sweep']
+           'Mod_Validate_Sweep',
+           'Comparison']
 
-from pyugend.pyugend.Models import Base_model
-from pyugend.pyugend.ReplicationModel import Replication_model
-from pyugend.pyugend.Mod_Stoch_FBHP import Mod_Stoch_FBHP
-from pyugend.pyugend.Mod_Stoch_FBPH import Mod_Stoch_FBPH
-from pyugend.pyugend.Mod_Validate_Sweep import Mod_Validate_Sweep
-from pyugend.pyugend.Comparison import Comparison
+from pyugend.Models import Base_model
+from pyugend.ReplicationModel import Replication_model
+from pyugend.Mod_Stoch_FBHP import Mod_Stoch_FBHP
+from pyugend.Mod_Stoch_FBPH import Mod_Stoch_FBPH
+from pyugend.Mod_Validate_Sweep import Mod_Validate_Sweep
+from pyugend.Comparison import Comparison
 
 
