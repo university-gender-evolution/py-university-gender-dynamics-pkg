dfbaf7496e3ec4de67d12e4d033f3678bac26ab1
krishnab <cyclotomiq@gmail.com>
2017-09-18 13:48:13 -0700

    developed new gender diversity model using model mark 2

---
+++ pyugend/ModelGenderDiversity.py
@@ -0,0 +1,272 @@
+"""
+Stochastic Model Gender Diversity
+---------------------
+
+This is the second generation of the gender diversity model.
+
+
+
+
+
+Notes:
+09/14/2017 - This is the first version of the second generate model.
+"""
+__author__ = 'krishnab'
+__version__ = '0.1.0'
+import numpy as np
+import pandas as pd
+from numpy.random import binomial, multinomial
+from .Models import Base_model
+
+MODEL_RUN_COLUMNS = list(['number_f1',
+                          'number_f2',
+                          'number_f3',
+                          'number_m1',
+                          'number_m2',
+                          'number_m3',
+                          'vacancies_3',
+                          'vacancies_2',
+                          'vacancies_1',
+                          'prom1',
+                          'prom2',
+                          'gender_proportion_overall',
+                          'unfilled_vacancies',
+                          'department_size',
+                          'f_hire_3',
+                          'm_hire_3',
+                          'f_hire_2',
+                          'm_hire_2',
+                          'f_hire_1',
+                          'm_hire_1',
+                          'f_prom_3',
+                          'm_prom_3',
+                          'f_prom_2',
+                          'm_prom_2',
+                          'f_prom_1',
+                          'm_prom_1'])
+
+EXPORT_COLUMNS_FOR_CSV = list(['hiring_rate_women_1',
+                               'hiring_rate_women_2',
+                               'hiring_rate_women_3',
+                               'hiring_rate_men_1',
+                               'hiring_rate_men_2',
+                               'hiring_rate_men_3',
+                               'attrition_rate_women_1',
+                               'attrition_rate_women_2',
+                               'attrition_rate_women_3',
+                               'attrition_rate_men_1',
+                               'attrition_rate_men_2',
+                               'attrition_rate_men_3',
+                               'probablity_of_outside_hire_1',
+                               'probability_of_outside_hire_2',
+                               'probability_of_outside_hire_3',
+                               'female_promotion_rate_1',
+                               'female_promotion_rate_2',
+                               'male_promotion_rate_1',
+                               'male_promotion_rate_2',
+                               'dept_size_upperbound',
+                               'dept_size_lowerbound',
+                               'dept_size_exogenous_variation_range',
+                               'duration'])
+
+
+class Model2GenderDiversity(Base_model):
+    def __init__(self, **kwds):
+        Base_model.__init__(self, **kwds)
+        self.name = "model-2-baseline"
+        self.label = "model-2-baseline"
+
+    def run_model(self):
+
+        # initialize data structure
+
+        self.res = np.zeros([self.duration,
+                            len(MODEL_RUN_COLUMNS) +
+                            len(EXPORT_COLUMNS_FOR_CSV)],
+                            dtype=np.float32)
+        self.res[0, 0] = self.nf1
+        self.res[0, 1] = self.nf2
+        self.res[0, 2] = self.nf3
+        self.res[0, 3] = self.nm1
+        self.res[0, 4] = self.nm2
+        self.res[0, 5] = self.nm3
+        self.res[0, 6] = self.vac3
+        self.res[0, 7] = self.vac2
+        self.res[0, 8] = self.vac1
+        self.res[0, 9] = self.female_promotion_probability_1
+        self.res[0, 10] = self.female_promotion_probability_2
+        self.res[0, 11] = self.res[0, 0:2]/self.res[0, 0:5]
+        self.res[0, 12] = 0
+        self.res[0, 13] = self.res[0, 0:6].sum()
+        self.res[0, 14:] = 0
+
+        # I assign the state variables to temporary variables. That way I
+        # don't have to worry about overwriting the original state variables.
+
+        hiring_rate_female_level_1 = self.bf1
+        hiring_rate_female_level_2 = self.bf2
+        hiring_rate_female_level_3 = self.bf3
+        hiring_rate_male_1 = 24/40
+        hiring_rate_male_2 = 3/40
+        hiring_rate_male_3 = 5/40
+        attrition_rate_female_level_1 = self.df1
+        attrition_rate_female_level_2 = self.df2
+        attrition_rate_female_level_3 = self.df3
+        attrition_rate_male_level_1 = self.dm1
+        attrition_rate_male_level_2 = self.dm2
+        attrition_rate_male_level_3 = self.dm3
+        female_promotion_probability_1_2 = self.female_promotion_probability_1
+        female_promotion_probability_2_3 = self.female_promotion_probability_2
+        department_size_upper_bound = self.upperbound
+        department_size_lower_bound = self.lowerbound
+        variation_range = self.variation_range
+        unfilled_vacanies = 0
+
+        for i in range(1, self.duration):
+            # initialize variables for this iteration
+
+            prev_number_of_females_level_1 = self.res[i - 1, 0]
+            prev_number_of_females_level_2 = self.res[i - 1, 1]
+            prev_number_of_females_level_3 = self.res[i - 1, 2]
+            prev_number_of_males_level_1 = self.res[i - 1, 3]
+            prev_number_of_males_level_2 = self.res[i - 1, 4]
+            prev_number_of_males_level_3 = self.res[i - 1, 5]
+            department_size = self.res[i - 1, 0:6].sum()
+            # Process Model
+            # attrition process
+            female_attrition_level_1 = binomial(prev_number_of_females_level_1,
+                                                attrition_rate_female_level_1)
+            female_attrition_level_2 = binomial(prev_number_of_females_level_2,
+                                                attrition_rate_female_level_2)
+            female_attrition_level_3 = binomial(prev_number_of_females_level_3,
+                                                attrition_rate_female_level_3)
+            male_attrition_level_1 = binomial(prev_number_of_males_level_1,
+                                              attrition_rate_male_level_1)
+            male_attrition_level_2 = binomial(prev_number_of_males_level_2,
+                                              attrition_rate_male_level_2)
+            male_attrition_level_3 = binomial(prev_number_of_males_level_3,
+                                              attrition_rate_male_level_3)
+            # update model numbers
+            self.res[i, 0] = self.res[i-1, 0] - female_attrition_level_1
+            self.res[i, 1] = self.res[i-1, 1] - female_attrition_level_2
+            self.res[i, 2] = self.res[i-1, 2] - female_attrition_level_3
+            self.res[i, 3] = self.res[i-1, 3] - male_attrition_level_1
+            self.res[i, 4] = self.res[i-1, 4] - male_attrition_level_2
+            self.res[i, 5] = self.res[i-1, 5] - male_attrition_level_3
+
+            # get total number of vacancies based on attrition
+            subtotal_vacancies_1 = female_attrition_level_1 \
+                + male_attrition_level_1
+            subtotal_vacancies_2 = female_attrition_level_2 \
+                + male_attrition_level_2
+            subtotal_vacancies_3 = female_attrition_level_3 \
+                + male_attrition_level_3
+            total_vacancies = subtotal_vacancies_3 \
+                + subtotal_vacancies_2 + subtotal_vacancies_1
+
+            # process promotions
+            promotions_of_females_level_2_3 = binomial(self.res[i, 1],
+                                        female_promotion_probability_2_3)
+            promotions_of_males_level_2_3 = binomial(self.res[i, 4],
+                                        1 - female_promotion_probability_2_3)
+            promotions_of_females_level_1_2 = binomial(self.res[i, 0],
+                                        female_promotion_probability_1_2)
+            promotions_of_males_level_1_2 = binomial(self.res[i, 3],
+                                    1 - female_promotion_probability_1_2)
+
+            # update model numbers
+            # add promotions to levels
+            self.res[i, 1] += promotions_of_females_level_1_2
+            self.res[i, 2] += promotions_of_females_level_2_3
+            self.res[i, 4] += promotions_of_males_level_1_2
+            self.res[i, 5] += promotions_of_males_level_2_3
+
+            # remove the promoted folks from previous level 
+            self.res[i, 0] -= promotions_of_females_level_1_2
+            self.res[i, 1] -= promotions_of_females_level_2_3
+            self.res[i, 3] -= promotions_of_males_level_1_2
+            self.res[i, 4] -= promotions_of_males_level_2_3
+
+            # hiring of new faculty
+            hires = multinomial(total_vacancies,
+                              [hiring_rate_female_level_1,
+                               hiring_rate_female_level_2,
+                               hiring_rate_female_level_3,
+                               hiring_rate_male_1,
+                               hiring_rate_male_2,
+                               hiring_rate_male_3])
+
+            self.res[i, 0] += hires[0]
+            self.res[i, 1] += hires[1]
+            self.res[i, 2] += hires[2]
+            self.res[i, 3] += hires[3]
+            self.res[i, 4] += hires[4]
+            self.res[i, 5] += hires[5]
+
+            # fill in summary info for model run
+
+            self.res[i, 6] = sum(list([
+                male_attrition_level_3,
+                female_attrition_level_3]))
+
+            self.res[i, 7] = sum(list([
+                male_attrition_level_2,
+                female_attrition_level_2,
+                promotions_of_females_level_2_3,
+                promotions_of_males_level_2_3]))
+
+            self.res[i, 8] = sum(list([
+                male_attrition_level_1,
+                female_attrition_level_1,
+                promotions_of_males_level_1_2,
+                promotions_of_females_level_1_2]))
+
+            self.res[i, 9] = self.female_promotion_probability_1
+            self.res[i, 10] = self.female_promotion_probability_1_2
+            self.res[i, 11] = self.res[i, 0:2].sum()/self.res[i,3:5].sum()  
+            unfilled_vacanies = abs(department_size - self.res[i, 0:6].sum())
+            self.res[i, 12] = unfilled_vacanies
+            department_size = self.res[i, 0:6].sum()
+            self.res[i, 13] = department_size
+            self.res[i, 14] = hires[2]
+            self.res[i, 15] = hires[5]
+            self.res[i, 16] = hires[1]
+            self.res[i, 17] = hires[4]
+            self.res[i, 18] = hires[0]
+            self.res[i, 19] = hires[3]
+            self.res[i, 20] = 0
+            self.res[i, 21] = 0
+            self.res[i, 22] = promotions_of_females_level_2_3
+            self.res[i, 23] = promotions_of_males_level_2_3
+            self.res[i, 24] = promotions_of_females_level_1_2
+            self.res[i, 25] = promotions_of_males_level_1_2
+            self.res[i, 26] = hiring_rate_female_level_1
+            self.res[i, 27] = hiring_rate_female_level_2
+            self.res[i, 28] = hiring_rate_female_level_3
+            self.res[i, 29] = 1 - hiring_rate_female_level_1
+            self.res[i, 30] = 1 - hiring_rate_female_level_2
+            self.res[i, 31] = 1 - hiring_rate_female_level_3
+            self.res[i, 32] = attrition_rate_female_level_1
+            self.res[i, 33] = attrition_rate_female_level_2
+            self.res[i, 34] = attrition_rate_female_level_3
+            self.res[i, 35] = attrition_rate_male_level_1
+            self.res[i, 36] = attrition_rate_male_level_2
+            self.res[i, 37] = attrition_rate_male_level_3
+            self.res[i, 38] = 1
+            self.res[i, 39] = 1
+            self.res[i, 40] = 1
+            self.res[i, 41] = female_promotion_probability_1_2
+            self.res[i, 42] = female_promotion_probability_2_3
+            self.res[i, 43] = 1 - female_promotion_probability_1_2
+            self.res[i, 44] = 1 - female_promotion_probability_2_3
+            self.res[i, 45] = department_size_upper_bound
+            self.res[i, 46] = department_size_lower_bound
+            self.res[i, 47] = variation_range
+            self.res[i, 48] = self.duration
+        df_ = pd.DataFrame(self.res)
+        df_.columns = MODEL_RUN_COLUMNS + EXPORT_COLUMNS_FOR_CSV
+
+        # print(df_)
+        recarray_results = df_.to_records(index=True)
+        self.run = recarray_results
+        return recarray_results
