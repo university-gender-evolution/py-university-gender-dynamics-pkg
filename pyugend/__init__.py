9ac1d0c023724197b99bdc63259a8412c94a6429
krishnab <cyclotomiq@gmail.com>
2017-10-03 17:16:13 -0400

    updated testing suite to check new model data export.

--- pyugend/__init__.py
+++ pyugend/__init__.py
@@ -1,23 +1,19 @@
-__version__ = '0.1.0'
+__version__ = '0.6.0'
 __all__ = ['Base_model',
            'Replication_model',
            'Mod_Stoch_FBHP',
            'Mod_Stoch_FBPH',
            'Mod_Validate_Sweep',
-           'Comparison']
+           'Comparison',
+           'Model2GenderDiversity']
 
-# import sys, os
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-#import os, sys
-#sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-# os.pardir))
 from .Models import Base_model
 from .ReplicationModel import Replication_model
 from .Mod_Stoch_FBHP import Mod_Stoch_FBHP
 from .Mod_Stoch_FBPH import Mod_Stoch_FBPH
 from .Mod_Validate_Sweep import Mod_Validate_Sweep
 from .Comparison import Comparison
-
+from .ModelGenderDiversity import Model2GenderDiversity
 
 
 
